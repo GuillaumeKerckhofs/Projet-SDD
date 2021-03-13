ebc22ccec6058bae2e170cc6e6f5c9b472cbf0fb
DarCryx <56272375+DarCryx@users.noreply.github.com>
2021-03-13 15:47:16 +0100

    update Point + Segment

--- Point.java
+++ Point.java
@@ -1,2 +1,46 @@
 public class Point {
+    private int x;
+    private int y;
+
+    private boolean upper_point;
+
+    public Point(int x,int y,boolean upper_point=false){
+        this.x=x;
+        this.y=y;
+        this.upper_point=upper_point;
+    }
+
+    public boolean smallerThan(Point point2){
+        int x2=point2.getX();
+        int y2=point2.getY();
+        if(this.y>y2 || (this.y==y2 && this.x<x2)){
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public void setUpper_point(boolean upper_point) {
+        this.upper_point = upper_point;
+    }
+
+    public boolean isUpper_point() {
+        return upper_point;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
--- Segment.java
+++ Segment.java
@@ -1,14 +1,36 @@
 public class Segment{
-    private int x1;
-    private int y1;
+    private Point upper_point;
+    private Point lower_point;
 
-    private int x2;
-    private int y2;
-    public Segment(String points[]){
-        this.x1=Integer.parseInt(points[0]);
-        this.y1=Integer.parseInt(points[1]);
+    public Segment(Point upper_point,Point lower_point){
+        this.upper_point=upper_point;
+        this.lower_point=lower_point;
+    }
 
-        this.x2=Integer.parseInt(points[2]);
-        this.y2=Integer.parseInt(points[3]);
+    public Segment(int x1,int y1,int x2,int y2){
+        if(y1>y2 || (y1==y2 && x1<x2)){
+            this.upper_point=new Point(x1,y1,true);
+            this.lower_point=new Point (x2,y2);
+        }
+        else{
+            this.upper_point=new Point(x2,y2,true);
+            this.lower_point=new Point (x1,y1);
+        }
+    }
+
+    public Point getUpper_point(){
+        return this.upper_point;
+    }
+
+    public Point getLower_point() {
+        return lower_point;
+    }
+
+    public void setLower_point(Point lower_point) {
+        this.lower_point = lower_point;
+    }
+
+    public void setUpper_point(Point upper_point) {
+        this.upper_point = upper_point;
     }
 }
